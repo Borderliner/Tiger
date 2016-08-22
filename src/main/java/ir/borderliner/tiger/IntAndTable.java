8da2007316fe5619ccdf29303536b3ba62bda869
Borderliner <hajianpour.mr@gmail.com>
2016-08-23 00:15:54 +0430

    Added IntAndTable class

---
+++ src/main/java/ir/borderliner/tiger/IntAndTable.java
@@ -0,0 +1,16 @@
+package ir.borderliner.tiger;
+
+/**
+ * Created by borderliner on 8/23/16.
+ */
+public class IntAndTable {
+
+    int i;
+    Table t;
+
+    public IntAndTable(int ii, Table tt) {
+        i = ii;
+        t = tt;
+    }
+
+}
